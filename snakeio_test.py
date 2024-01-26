860276c094b26f39330005a1bdd8df86cefee376
Britney Shepherd <brta.shp@gmail.com>
2024-01-26 11:04:08 -0500

    next update
    still no "food"

--- snakeio_test.py
+++ snakeio_test.py
@@ -22,9 +22,25 @@ clock = pygame.time.Clock()
 
 font_style = pygame.font.SysFont(None, 50)
 
+vocab_terms = {
+    "Ecosystem": "A biological community of interacting organisms and their physical environment.",
+    "Mitosis": "A type of cell division that results in two daughter cells each having the same number and kind of chromosomes as the parent nucleus.",
+    "Osmosis": "A process by which molecules of a solvent pass through a semipermeable membrane from a less concentrated solution into a more concentrated one.",
+    # Add more terms and definitions here
+}
+
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     game_display.blit(mesg, [width / 6, height / 3])
+    
+def draw_vocab_food(food_x, food_y, food_word):
+    pygame.draw.rect(game_display, red, [food_x, food_y, snake_block, snake_block])
+    food_text = font_style.render(food_word, True, black)
+    game_display.blit(food_text, [food_x, food_y])
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(game_display, white, [x[0], x[1], snake_block, snake_block])
 
 def game_loop():
     game_over = False
@@ -37,8 +53,26 @@ def game_loop():
     x1_change = 0       
     y1_change = 0
 
+    current_definition, current_word = random.choice(list(vocab_terms.items()))
+    word_choices = list(vocab_terms.keys())
+    random.shuffle(word_choices)
+
+    # Position for vocab 'food'
+    food_x = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+    food_y = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+
     # Main game loop
     while not game_over:
+
+        # Display current definition
+        definition_text = font_style.render(current_definition, True, white)
+        game_display.blit(definition_text, [50, 10])
+
+        # Draw vocab 'food'
+        for word in word_choices[:3]:  # Displaying three options
+            draw_vocab_food(food_x, food_y, word)
+            food_x += 70  # Adjust position for next word
+
 
         while game_close:
             game_display.fill(white)
